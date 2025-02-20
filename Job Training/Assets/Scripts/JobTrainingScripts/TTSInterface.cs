1f200f2a35fc90fe14dd5916b44d1c3c76152715
Domenico Popolizio <domenico.popolizio@mail.polimi.it>
2025-02-20 15:41:17 +0100

    Update TTSInterface.cs

--- Job Training/Assets/Scripts/JobTrainingScripts/TTSInterface.cs
+++ Job Training/Assets/Scripts/JobTrainingScripts/TTSInterface.cs
@@ -1,28 +1,31 @@
 
 
-using UnityEngine; 
-using UnityEngine.Networking; 
+using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 using System.Linq;
 public class TTSInterface : MonoBehaviour
 {
     public event OnTTSPlaying TTsPlaying;
-    public void PlayAudio(string text){ //lookup how it works with Secco once ready
+    public void PlayAudio(string text)
+    { //lookup how it works with Secco once ready
         MagicRoomManager.instance.MagicRoomTextToSpeachManager.GenerateAudioFromText(text);
         TTsPlaying?.Invoke(ApproximateTimeToSpeach(text));
-       
+
     }
-    public void PlayAudio(string text, string voice){
+    public void PlayAudio(string text, string voice = "Magika")
+    {
         //foreach(Voices v in MagicRoomManager.instance.MagicRoomTextToSpeachManager.ListOfVoice)
-          //  Debug.Log(v.alias);
-        //Voices vv = MagicRoomManager.instance.MagicRoomTextToSpeachManager.ListOfVoice.FirstOrDefault(x => x.alias == "VE_Italian_Luca_22kHz");
-        MagicRoomManager.instance.MagicRoomTextToSpeachManager.GenerateAudioFromText(text);
+        //  Debug.Log(v.alias);
+        Voices vv = MagicRoomManager.instance.MagicRoomTextToSpeachManager.ListOfVoice.FirstOrDefault(x => x.alias == voice);
+        MagicRoomManager.instance.MagicRoomTextToSpeachManager.GenerateAudioFromText(text, vv);
         TTsPlaying?.Invoke(ApproximateTimeToSpeach(text));
     }
-    public float ApproximateTimeToSpeach(string text){
-        int words=text.Split().Length;
-        float secondsPerWord=0.6f;
-        return words*secondsPerWord;
+    public float ApproximateTimeToSpeach(string text)
+    {
+        int words = text.Split().Length;
+        float secondsPerWord = 0.6f;
+        return words * secondsPerWord + 4;
     }
 }
-    public delegate void OnTTSPlaying(float lenghtInSeconds);
+public delegate void OnTTSPlaying(float lenghtInSeconds);
